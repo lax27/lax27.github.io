c7acae12b2d5d3961de7552cb0c3965dff3df000
pc casa <alex.taravilla@enti.cat>
2023-05-27 17:54:57 +0200

    no se ve la descripcion

--- game.js
+++ game.js
@@ -69,7 +69,6 @@ console.log("La instrucion", instruction);
 			let item_description = game_data.items[item_number].description;
 			
 			terminal_out("<p><strong>" + instruction[1] + ":</strong> " + item_description + "</p>");
-			
 			break;
 		
 		
